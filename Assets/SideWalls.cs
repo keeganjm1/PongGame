a27702fe846a658a607b50929813ea5fb9a9ffe8
keeganjm1 <43150767+keeganjm1@users.noreply.github.com>
2019-01-10 21:51:01 -0600

    Add files via upload

---
+++ Assets/SideWalls.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SideWalls : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D hitinfo)
+    {
+         if (hitinfo.name == "Ball")
+        {
+            string wallName = transform.name;
+            GameManager.Score(wallName);
+            hitinfo.gameObject.SendMessage("RestartGame", 1.0f, SendMessageOptions.RequireReceiver);
+                
+        }
+    }
+
+}
